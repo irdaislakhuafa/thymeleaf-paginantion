774d6245ca439b08884be6c330c06f52cb5faa69
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-01-22 12:20:47 +0700

    First commit

---
+++ src/main/java/com/irdaislakhuafa/thymeleafpaginantion/ThymeleafPaginantionApplication.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.thymeleafpaginantion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafPaginantionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafPaginantionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irdaislakhuafa/thymeleafpaginantion/ThymeleafPaginantionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irdaislakhuafa.thymeleafpaginantion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafPaginantionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
