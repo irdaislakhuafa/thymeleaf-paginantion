8f3b482eb2a23b6f104c5fa4359d6b2ac6bc18c6
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-01-24 13:56:23 +0700

    add pageable response example

--- src/main/java/com/irdaislakhuafa/thymeleafpaginantion/controllers/PageableResponseController.java
+++ src/main/java/com/irdaislakhuafa/thymeleafpaginantion/controllers/PageableResponseController.java
@@ -1,32 +1,33 @@
 package com.irdaislakhuafa.thymeleafpaginantion.controllers;
 
-import java.util.List;
-
 import com.irdaislakhuafa.thymeleafpaginantion.models.BookEntity;
 import com.irdaislakhuafa.thymeleafpaginantion.services.BookService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
-@RequestMapping("/thymeleaf/pagination")
-public class BookController {
+@RestController
+@RequestMapping("/thymeleaf/pagetest")
+public class PageableResponseController {
     @Autowired
-    private BookService bookService;
+    BookService bookService;
 
     @GetMapping
-    public ResponseEntity<List<BookEntity>> getAll() {
+    public ResponseEntity<?> getAll() {
         try {
-            List<BookEntity> items = bookService.findAll();
 
-            if (items.isEmpty())
+            Page<BookEntity> bookPage = bookService.findAll(PageRequest.of(0, 5));
+
+            if (bookPage.isEmpty())
                 return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 
-            return new ResponseEntity<>(items, HttpStatus.OK);
+            return new ResponseEntity<>(bookPage, HttpStatus.OK);
         } catch (Exception e) {
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
