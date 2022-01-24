143e37b8805cec37e03359032f28af077c5af264
Irda Islakhu Afa <irdhaislakhuafa@gmail.com>
2022-01-24 15:24:53 +0700

    add pagination and view

--- src/main/java/com/irdaislakhuafa/thymeleafpaginantion/controllers/BookController.java
+++ src/main/java/com/irdaislakhuafa/thymeleafpaginantion/controllers/BookController.java
@@ -1,16 +1,22 @@
 package com.irdaislakhuafa.thymeleafpaginantion.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import com.irdaislakhuafa.thymeleafpaginantion.models.BookEntity;
 import com.irdaislakhuafa.thymeleafpaginantion.services.BookService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/thymeleaf/pagination")
@@ -19,16 +25,35 @@ public class BookController {
     private BookService bookService;
 
     @GetMapping
-    public ResponseEntity<List<BookEntity>> getAll() {
+    public String index(
+            Model model,
+            @RequestParam("requestPage") Optional<Integer> requestPage,
+            @RequestParam("requestSize") Optional<Integer> requestSize) {
         try {
-            List<BookEntity> items = bookService.findAll();
+            // get books pageable
+            Page<BookEntity> bookPages = bookService.findAll(
+                    PageRequest.of(
+                            requestPage.orElse(0),
+                            requestSize.orElse(10),
+                            Sort.by("name").ascending()));
 
-            if (items.isEmpty())
-                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+            List<Integer> bookPageNumbers = IntStream.rangeClosed(
+                    1, bookPages.getTotalPages())
+                    // convert to Stream<Integer>
+                    .boxed()
+                    // convert to list
+                    .collect(Collectors.toList());
 
-            return new ResponseEntity<>(items, HttpStatus.OK);
+            // System.out.println(IntStream.rangeClosed(1,
+            // 3).boxed().collect(Collectors.toList()));
+
+            model.addAttribute("title", "Spring Boot Thymeleaf | Pagination");
+            model.addAttribute("contentTitle", "Book Table");
+            model.addAttribute("bookPage", bookPages);
+            model.addAttribute("bookPageNumbers", bookPageNumbers);
+            return "index";
         } catch (Exception e) {
-            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+            throw new RuntimeException("File view not found!");
         }
     }
 
